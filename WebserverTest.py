19c9425b0055f3ae69b02b4a60fe7b28ac3bee6c
ridolfil <ridolfi.luc@gmail.com>
2018-10-14 16:51:40 +0100

    deleted files

--- WebserverTest.py
+++ WebserverTest.py
@@ -5,7 +5,7 @@ app = Flask(__name__,static_url_path="/WS_UI")
 @app.route('/')
 def index():
     return send_from_directory("WS_UI","index.html")
-
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
